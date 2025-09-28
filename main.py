27a5affea08c639e3a664afb101e6d1b7074dada
ralv76 <133973999+ralv76@users.noreply.github.com>
2025-09-29 01:19:05 +0300

    PS05-06
    PS05-06

--- main.py
+++ main.py
@@ -15,16 +15,20 @@ class DivanSpider(scrapy.Spider):
     def parse(self, response):
         lights = response.css('div._Ud0k')
         for light in lights:
+            price = light.css('div.pY3d2 span::text').get()
+            if price:
+                price = int(price.replace(' ', '').replace('₽', ''))
+            
             yield {
-                'name': light.css('div.lsooF span::text').get(),
-                'price': light.css('div.pY3d2 span::text').get(),
+                'name': light.css('div.lsooF span::text').get().encode('utf-8'),
+                'price': price,
                 'url': 'https://www.divan.ru' + light.css('a').attrib['href']
             }
 
 if __name__ == "__main__":
     process = CrawlerProcess(settings={
-        'FEED_FORMAT': 'json',
-        'FEED_URI': 'result.txt'
+        'FEED_FORMAT': 'csv',
+        'FEED_URI': 'result_3.csv'
     })
     process.crawl(DivanSpider)
     process.start()
